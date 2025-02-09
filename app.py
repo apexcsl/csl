ab809d5244cbb0d29d3c85d0afc2c20dcd88af16
Daniel Tenorio <danyjr@outlook.es>
2025-02-09 17:20:58 -0600

    fixed

--- app.py
+++ app.py
@@ -3,8 +3,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-  return render_template('index.html')
+def index():
+  return render_template('home.html')
+
+@app.route('/home')
+def home ():
+  return render_template('home.html')
 
 @app.route('/login')
 def login():
